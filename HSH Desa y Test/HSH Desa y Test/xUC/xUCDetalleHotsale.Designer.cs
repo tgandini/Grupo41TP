0faa1c8686f46c20f98aaf0d7c25305b253e8c39
Mauricio Agosta <mauriciobostero96@hotmail.com>
2019-07-09 13:06:43 -0300

    hotsale

---
+++ HSH Desa y Test/HSH Desa y Test/xUC/xUCDetalleHotsale.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace HSH_Desa_y_Test.xUC
+{
+    partial class xUCDetalleHotsale
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nombreLabel = new System.Windows.Forms.Label();
+            this.ubicacionLabel = new System.Windows.Forms.Label();
+            this.montoLabel = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.ciudadLabel = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.fechaLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ubicacion:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Monto:";
+            // 
+            // nombreLabel
+            // 
+            this.nombreLabel.Location = new System.Drawing.Point(131, 52);
+            this.nombreLabel.Name = "nombreLabel";
+            this.nombreLabel.Size = new System.Drawing.Size(454, 20);
+            this.nombreLabel.TabIndex = 3;
+            this.nombreLabel.Text = "label4";
+            this.nombreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // ubicacionLabel
+            // 
+            this.ubicacionLabel.Location = new System.Drawing.Point(131, 122);
+            this.ubicacionLabel.Name = "ubicacionLabel";
+            this.ubicacionLabel.Size = new System.Drawing.Size(447, 20);
+            this.ubicacionLabel.TabIndex = 4;
+            this.ubicacionLabel.Text = "label5";
+            this.ubicacionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // montoLabel
+            // 
+            this.montoLabel.Location = new System.Drawing.Point(131, 196);
+            this.montoLabel.Name = "montoLabel";
+            this.montoLabel.Size = new System.Drawing.Size(450, 20);
+            this.montoLabel.TabIndex = 5;
+            this.montoLabel.Text = "label6";
+            this.montoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(8, 86);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 21);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Ciudad:";
+            // 
+            // ciudadLabel
+            // 
+            this.ciudadLabel.Location = new System.Drawing.Point(131, 85);
+            this.ciudadLabel.Name = "ciudadLabel";
+            this.ciudadLabel.Size = new System.Drawing.Size(447, 20);
+            this.ciudadLabel.TabIndex = 7;
+            this.ciudadLabel.Text = "label7";
+            this.ciudadLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(8, 159);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 21);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Fecha:";
+            // 
+            // fechaLabel
+            // 
+            this.fechaLabel.Location = new System.Drawing.Point(131, 158);
+            this.fechaLabel.Name = "fechaLabel";
+            this.fechaLabel.Size = new System.Drawing.Size(447, 20);
+            this.fechaLabel.TabIndex = 9;
+            this.fechaLabel.Text = "label10";
+            this.fechaLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.fechaLabel);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.ciudadLabel);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.montoLabel);
+            this.groupBox1.Controls.Add(this.ubicacionLabel);
+            this.groupBox1.Controls.Add(this.nombreLabel);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(3, 26);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(407, 244);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Detalles";
+            // 
+            // xUCDetalleHotsale
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Name = "xUCDetalleHotsale";
+            this.Size = new System.Drawing.Size(961, 318);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label nombreLabel;
+        private System.Windows.Forms.Label ubicacionLabel;
+        private System.Windows.Forms.Label montoLabel;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label ciudadLabel;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label fechaLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ HSH Desa y Test/HSH Desa y Test/xUC/xUCDetalleHotsale.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using HSH_Desa_y_Test.ContextoDB;
+
+namespace HSH_Desa_y_Test.xUC
+{
+    public partial class xUCDetalleHotsale : DevExpress.XtraEditors.XtraUserControl
+    {
+        public xUCDetalleHotsale()
+        {
+            InitializeComponent();
+        }
+
+        public void inicializar(HotSale hot, Propiedad prop)
+        {
+            nombreLabel.Text = prop.nombre;
+            ciudadLabel.Text = prop.ciudad;
+        }
+    }
+}
