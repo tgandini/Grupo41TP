fb7df2df70f85a1e94b26554a987976b5723fc9b
Tomas Gandini <gandinitomas@gmail.com>
2019-07-09 14:41:06 -0300

    agregado filtro listado hotsales

--- HSH Desa y Test/HSH Desa y Test/xUC/xUCListadoHotSale.Designer.cs
+++ HSH Desa y Test/HSH Desa y Test/xUC/xUCListadoHotSale.Designer.cs
@@ -42,10 +42,12 @@
             // gridControl1
             // 
             this.gridControl1.DataSource = typeof(HSH_Desa_y_Test.ContextoDB.HotSale);
-            this.gridControl1.Location = new System.Drawing.Point(5, 5);
+            this.gridControl1.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gridControl1.Location = new System.Drawing.Point(4, 4);
             this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gridControl1.Name = "gridControl1";
-            this.gridControl1.Size = new System.Drawing.Size(566, 646);
+            this.gridControl1.Size = new System.Drawing.Size(485, 525);
             this.gridControl1.TabIndex = 0;
             this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
@@ -57,61 +59,70 @@
             this.colmonto,
             this.colsemanaReservada,
             this.colPropiedad});
+            this.gridView1.DetailHeight = 284;
             this.gridView1.GridControl = this.gridControl1;
             this.gridView1.Name = "gridView1";
             this.gridView1.OptionsBehavior.Editable = false;
+            this.gridView1.OptionsFind.AlwaysVisible = true;
+            this.gridView1.OptionsFind.FindDelay = 300;
+            this.gridView1.OptionsFind.FindMode = DevExpress.XtraEditors.FindMode.Always;
+            this.gridView1.OptionsFind.FindNullPrompt = "Introduzca para filtar";
+            this.gridView1.OptionsFind.ShowCloseButton = false;
+            this.gridView1.OptionsFind.ShowFindButton = false;
             // 
             // colfechaInicio
             // 
             this.colfechaInicio.FieldName = "fechaInicio";
-            this.colfechaInicio.MinWidth = 25;
+            this.colfechaInicio.MinWidth = 21;
             this.colfechaInicio.Name = "colfechaInicio";
             this.colfechaInicio.Visible = true;
             this.colfechaInicio.VisibleIndex = 0;
-            this.colfechaInicio.Width = 94;
+            this.colfechaInicio.Width = 81;
             // 
             // colmonto
             // 
             this.colmonto.FieldName = "monto";
-            this.colmonto.MinWidth = 25;
+            this.colmonto.MinWidth = 21;
             this.colmonto.Name = "colmonto";
             this.colmonto.Visible = true;
             this.colmonto.VisibleIndex = 1;
-            this.colmonto.Width = 94;
+            this.colmonto.Width = 81;
             // 
             // colsemanaReservada
             // 
             this.colsemanaReservada.FieldName = "semanaReservada";
-            this.colsemanaReservada.MinWidth = 25;
+            this.colsemanaReservada.MinWidth = 21;
             this.colsemanaReservada.Name = "colsemanaReservada";
             this.colsemanaReservada.Visible = true;
             this.colsemanaReservada.VisibleIndex = 2;
-            this.colsemanaReservada.Width = 94;
+            this.colsemanaReservada.Width = 81;
             // 
             // colPropiedad
             // 
             this.colPropiedad.FieldName = "Nombre";
-            this.colPropiedad.MinWidth = 25;
+            this.colPropiedad.MinWidth = 21;
             this.colPropiedad.Name = "colPropiedad";
             this.colPropiedad.Visible = true;
             this.colPropiedad.VisibleIndex = 3;
-            this.colPropiedad.Width = 94;
+            this.colPropiedad.Width = 81;
             // 
             // xUCDetalleHotsale1
             // 
-            this.xUCDetalleHotsale1.Location = new System.Drawing.Point(577, 109);
+            this.xUCDetalleHotsale1.Location = new System.Drawing.Point(495, 89);
+            this.xUCDetalleHotsale1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.xUCDetalleHotsale1.Name = "xUCDetalleHotsale1";
-            this.xUCDetalleHotsale1.Size = new System.Drawing.Size(799, 318);
+            this.xUCDetalleHotsale1.Size = new System.Drawing.Size(685, 258);
             this.xUCDetalleHotsale1.TabIndex = 1;
             // 
             // xUCListadoHotSale
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.xUCDetalleHotsale1);
             this.Controls.Add(this.gridControl1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "xUCListadoHotSale";
-            this.Size = new System.Drawing.Size(1379, 730);
+            this.Size = new System.Drawing.Size(1182, 593);
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             this.ResumeLayout(false);
